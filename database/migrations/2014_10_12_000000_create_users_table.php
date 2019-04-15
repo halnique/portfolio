d5e359826929911ef5b955c16d1028522fbeff84
halnique <shunsuke4dev@gmail.com>
2019-04-15 23:39:32 +0900

    add some change * timezone UTC->Asia/Tokyo * locale en->ja * MySQL collation utf8mb4_unicode_ci->utf8mb4_general_ci * MySQL timestamps->datetime * add laravel-ide-helper

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
