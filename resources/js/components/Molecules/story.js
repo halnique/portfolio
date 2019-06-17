de86308e2f9951fac81e2573fe35d1907320e052
halnique <shunsuke4dev@gmail.com>
2019-06-17 16:46:37 +0900

    add social area component

--- resources/js/components/Molecules/story.js
+++ resources/js/components/Molecules/story.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import HeaderCard from './HeaderCard';
+import SocialArea from './SocialArea';
 
 storiesOf('Molecules', module).add('HeaderCard', () => (
     <HeaderCard profile={{ name: 'name', iconUrl: '' }}/>
+)).add('SocialArea', () => (
+    <SocialArea profile={{
+        github: {
+            url: 'https://github.com/',
+        },
+        twitter: {
+            url: 'https://twitter.com/',
+        },
+        qiita: {
+            url: 'https://qiita.com/',
+        },
+        hatena: {
+            url: 'https://hatenablog.com/',
+        },
+    }}/>
 ));
