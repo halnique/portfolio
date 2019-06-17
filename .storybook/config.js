852b44c21cb3a9efec626bad0980d8feb6a8b263
halnique <shunsuke4dev@gmail.com>
2019-06-17 17:27:55 +0900

    fix stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,8 @@ import { configure } from '@storybook/react';
 function loadStories() {
     require('../resources/js/components/Atoms/story.js');
     require('../resources/js/components/Molecules/story.js');
+    require('../resources/js/components/Organisms/story.js');
+    require('../resources/js/components/Templates/story.js');
 }
 
 configure(loadStories, module);
