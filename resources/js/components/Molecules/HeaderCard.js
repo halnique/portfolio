49dff0a540cc731de00312ce77673824646ba78d
halnique <shunsuke4dev@gmail.com>
2019-06-17 16:46:51 +0900

    change to functional component

--- resources/js/components/Molecules/HeaderCard.js
+++ resources/js/components/Molecules/HeaderCard.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NameHeader from '../Atoms/NameHeader';
 import IconImage from '../Atoms/IconImage';
 
-export default class HeaderCard extends Component {
-    render() {
-        return (
-            <div className="card">
-                <NameHeader name={this.props.profile.name}/>
-                <IconImage iconUrl={this.props.profile.iconUrl}/>
-            </div>
-        );
-    }
-}
+const HeaderCard = (props) => (
+    <div className="card">
+        <NameHeader name={props.profile.name}/>
+        <IconImage iconUrl={props.profile.iconUrl}/>
+    </div>
+);
 
+export default HeaderCard;
